81462845eddb4133f2aa492ba1ce67922b0850cd
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-05-09 00:06:38 +0100

    connection & internal functions

--- src/data/dataset_funcs.py
+++ src/data/dataset_funcs.py
@@ -1,34 +1,49 @@
 import pandas as pd
+import psycopg2 as p2
+import os
 
 class DataManager:
     """
     This class is designed for loading, storing, and uploading datasets used in this project. These are kept in a database, where each 
-    project has its own schema. 
+    project has its own schema. Connections and use of a Postgresql database uses the psycopg2 package; for docs on this package, follow
+    this link: https://www.psycopg.org/docs/connection.html.
     
     These are unique to the database that I am working with, and unlikely to work for others, but can be 
     used as inspiration for those who work with Postgresql DBs in their python prjects!
     """
 
-    def __init__(self, connection_params):
-        self.connection_params = connection_params
-        self.connection = None
+    connection = None
+
+    def __init__(self):
+        self._connect()
 
 
     # Connection/Internal functions
     def _connect(self):
-        pass
+        try:
+            self.connection = p2.connect(
+                dbname=os.get_env("DB_NAME"),
+                user=os.get_env("DB_USER"),
+                password=os.get_env("DB_PASSWORD"),
+                host=os.get_env("DB_HOST")
+            )
+
+            print('Connection successful.')
+        
+        except (Exception, p2.DatabaseError) as e:
+            print(e)
 
     
     def _close(self):
-        pass
+        self.connection.close()
 
     
     def _commit(self):
-        pass
+        self.connection.commit()
 
 
     def _rollback(self):
-        pass
+        self.connection.rollback()
 
 
     # Metadata Functions
